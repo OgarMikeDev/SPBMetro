71aaeeedb7b02291f547495f24a5767b47f6ed6c
Misha <muraviov.m.v@mail.ru>
2022-08-22 14:49:38 +0300

    "spb-metro"

--- SPBMetro/SPBMetro/src/test/java/RouteCalculatorTest.java
+++ SPBMetro/SPBMetro/src/test/java/RouteCalculatorTest.java
@@ -2,6 +2,7 @@ import core.Line;
 import core.Station;
 import junit.framework.TestCase;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RouteCalculatorTest extends TestCase {
@@ -14,13 +15,38 @@ public class RouteCalculatorTest extends TestCase {
 
         routeForDuration = new ArrayList<>();
 
-        Line line1 = new Line(1, "One");
-        Line line2 = new Line(2, "Two");
+        Line line1 = new Line(1, "OneLine");
+        Line line2 = new Line(2, "TwoLine");
 
         routeForDuration.add(new Station("Петровская", line1));
         routeForDuration.add(new Station("Арбузная", line1));
         routeForDuration.add(new Station("Морковная", line2));
         routeForDuration.add(new Station("Яблочная", line2));
+
+        List<Line> lines = Arrays.asList(
+                new Line(1, "OneL"),
+                new Line(2, "TwoL"));
+
+        lines.forEach(line -> stationIndex.addLine(line));
+
+        List<Station> stations = Arrays.asList(
+                new Station("station_1.1", stationIndex.getLine(1)),
+                new Station("station_1.2", stationIndex.getLine(1)),
+
+                new Station("station_2.1", stationIndex.getLine(2)),
+                new Station("station_2.2", stationIndex.getLine(2))
+        );
+
+        stations.forEach(station -> {
+            station.getLine()
+                    .addStation(station);
+            stationIndex.addStation(station);
+        });
+
+        stationIndex.addConnection(Arrays.asList(
+                stationIndex.getStation("station_1.1"),
+                stationIndex.getStation("station_2.2")
+        ));
 
     }
 
@@ -32,15 +58,16 @@ public class RouteCalculatorTest extends TestCase {
     }
 
     public void testGetRouteOnTheLine() throws Exception {
-        List <Station> actual = List.of(
+
+        List<Station> actual = List.of(
                 stationIndex.getStation("station_1.1"),
                 stationIndex.getStation("station_1.2"),
-                stationIndex.getStation("station_1.3"),
-                stationIndex.getStation("station_1.4")
+                stationIndex.getStation("station_2.1"),
+                stationIndex.getStation("station_2.2")
         );
 
         List <Station> expected = calculator.getShortestRoute(stationIndex.getStation("station_1.1")
-        ,stationIndex.getStation("station_1.4"));
+        ,stationIndex.getStation("station_2.2"));
 
         assertEquals(expected, actual);
     }
